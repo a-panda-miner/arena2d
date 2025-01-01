9fe054ca99bcb945bec8eb425b4d5252e2f98208
a_panda_miner <fabiodsa7@gmail.com>
2025-01-01 10:06:07 -0300

    fixed shell.nix for rust-analyzer

--- shell.nix
+++ shell.nix
@@ -4,21 +4,11 @@ let
 	overlays = [ (import rustOverlay) ];
   };
 	rust = pkgs.rust-bin.stable.latest.default.override {
-  	extensions = [ "llvm-tools-preview" ];
+  	extensions = [ "llvm-tools-preview" "rust-src" "cargo" "rustc"];
 		targets = [ "wasm32-unknown-unknown" ];
 	};
 in
   pkgs.mkShell {
-    packages = [ 
-		pkgs.pkg-config
-		pkgs.alsa-lib
-		pkgs.libudev-zero
-		pkgs.xorg.libX11
-		pkgs.xorg.libXrandr
-		pkgs.xorg.libXcursor
-		pkgs.xorg.libXi
-		pkgs.libxkbcommon
-	];
     nativeBuildInputs = [ pkgs.pkg-config ];
     LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
 		pkgs.pkg-config
@@ -32,9 +22,19 @@ in
         pkgs.libglvnd	
 	];
 	buildInputs = with pkgs; [
-      rust
-      rust-analyzer
-			cargo-pgo
+		pkg-config
+		alsa-lib
+		xorg.libX11
+		xorg.libXrandr
+		xorg.libXcursor
+		xorg.libXi
+		libxkbcommon
+    rust
+    rust-analyzer
+		cargo-pgo
 	];
 	RUST_BACKTRACE = 1;
+	RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default.override {
+    extensions = [ "rust-src" ];
+  }}/lib/rustlib/src/rust/library";
 }
